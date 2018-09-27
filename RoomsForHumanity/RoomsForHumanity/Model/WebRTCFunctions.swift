898aeda7f14daf2f8a8e3e289da92396df4d6469
Jocelyn Ragukonis <jnragu@gmail.com>
2018-09-27 18:56:01 -0400

    initial push

---
+++ RoomsForHumanity/RoomsForHumanity/Model/WebRTCFunctions.swift
@@ -0,0 +1,16 @@
+//
+//  WebRTCFunctions.swift
+//  RoomsForHumanity
+//
+//  Created by Jocelyn Ragukonis on 9/21/18.
+//  Copyright © 2018 Jocelyn Ragukonis. All rights reserved.
+//
+
+import UIKit
+import WebRTC
+
+class WebRTCFunctions {
+    
+    var localStream: RTCMediaStream = [
+    
+}
